bf1b039fbc3e677457deb154ba9d8f4ff9688efe
Your Name <you@example.com>
2021-11-03 10:18:01 +0100

    update connect

--- src/pages/Test/connect.js
+++ src/pages/Test/connect.js
@@ -2,13 +2,14 @@ import {connect} from 'react-redux';
 
 import Test from './Test';
 import {fetchTestData} from './actions';
-import {testDataSelector, testLoadingSelector} from './selectors';
+import {testDataSelector, testLoadingSelector, testExampleSelector} from './selectors';
 export const mapStateToProps = (state) => ({
   testData: testDataSelector(state),
+  testExample: testExampleSelector(state),
   testLoading: testLoadingSelector(state),
 });
 
-const mapDispatchToProps = {
+export const mapDispatchToProps = {
   fetchTestData,
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Test);
