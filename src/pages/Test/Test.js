0b37c851d8fc2ef96bf1bc163609b6d930d2f1e4
Your Name <you@example.com>
2021-11-16 14:04:36 +0100

    work witch radioButtons

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,8 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Radio, {NativeRadioControl} from '@material/react-radio';
 import Skeleton from 'react-loading-skeleton';
+
+import '../../index.scss';
 
 import {shared} from '../../routesConstants';
 
@@ -11,7 +14,7 @@ import {fetchTestData} from './actions';
 import {testDataSelector, testLoadingSelector} from './selectors';
 const Test = () => {
   const dispatch = useDispatch();
-
+  const [content, setContent] = useState('');
   const testData = useSelector((state) => testDataSelector(state));
 
   const testLoadingExample = useSelector((state) => testLoadingSelector(state));
@@ -23,8 +26,7 @@ const Test = () => {
   const history = useHistory();
 
   const handleSwitch = () => history.push({pathname: shared.routes.mainPage.root});
-
-  const content = testLoadingExample ? (
+  const data = testLoadingExample ? (
     <Skeleton count={100} />
   ) : (
     <section data-testid="test-container">
@@ -37,15 +39,37 @@ const Test = () => {
             {user?.airline?.map((item) => (
               <p key={item.id}>
                 {item.name}--{item.head_quaters}
-                <br />
-                <img src={item.logo} alt="airlines" width="70" height="60" />
               </p>
             ))}
           </div>
         ))}
     </section>
   );
-  return <div>{content}</div>;
+  return (
+    <div>
+      <div>
+        <Radio label="show" key="data">
+          <NativeRadioControl
+            id="data"
+            checked={content === data}
+            onChange={() => {
+              setContent(data);
+            }}
+          />
+        </Radio>
+        <Radio label="hide" key="noData">
+          <NativeRadioControl
+            id="noData"
+            checked={content === ''}
+            onChange={() => {
+              setContent('');
+            }}
+          />
+        </Radio>
+      </div>
+      {content}
+    </div>
+  );
 };
 
 Test.propTypes = {
