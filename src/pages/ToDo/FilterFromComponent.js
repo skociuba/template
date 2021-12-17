153cd5e0c34725c5433fb48812be5a4c10a5921c
Your Name <you@example.com>
2021-12-17 20:52:16 +0100

    update filter from component

--- src/pages/ToDo/FilterFromComponent.js
+++ src/pages/ToDo/FilterFromComponent.js
@@ -3,41 +3,28 @@ import styled from 'styled-components';
 import {useSelector} from 'react-redux';
 
 import {testDataSelector} from './selectors';
-const Input = styled.div`
-  height: 200px;
-  margin-bottom: 10px;
-  border-bottom: 2px solid #ff5601;
-`;
-const Info2 = styled.div`
-  border: 2px solid white;
-  padding: 30px;
-  color: white;
 
-  min-padding: 10px;
+const Item = styled.div`
+  border: 2px solid white;
+  padding: 10px 30px 10px;
   border-radius: 8px;
-  margin: 10px;
-  background-color: rgba(0, 0, 0, 0.9);
+  background-color: #002f75;
+  @media (min-width: 800px) {
+    flex-direction: row;
+    padding-right: 0px;
+  }
 `;
 const StyledButton = styled.button`
-  background-color: #ff5601;
+  background-color: #226de6;
   border: 1px solid black;
   padding: 10px;
   margin: 15px;
   color: white;
   border-radius: 6px;
   &:hover {
-    background-color: black;
+    background-color: #00b38f;
     color: white;
   }
-  &:focus {
-    background-color: #ff5601;
-    color: white;
-  }
-`;
-const Container = styled.div`
-  border-top: 2px solid #ff5601;
-  text-align: center;
-  padding: 10px;
 `;
 
 const Filter = () => {
@@ -56,43 +43,39 @@ const Filter = () => {
 
   return (
     <Fragment>
-      <Container>
-        <Input>
-          <div className="form-group-collection">
-            <input
-              onChange={(e) => {
-                const test = example?.filter((item) => {
-                  return item.toLowerCase().includes(e.target.value.toLowerCase());
-                });
-                setData(test);
-                setSearch(e.target.value);
-              }}
-              type="text"
-              value={search}
-            />
-          </div>
-          <StyledButton type="reset" defaultValue="Reset" onClick={reset}>
-            Reset filter
-          </StyledButton>
-        </Input>
-        <Info2>
-          {' '}
-          <h3>My tasks:</h3>
-          {data?.length > 0 ? (
-            <ul>
-              {data.map((elem) => {
-                return (
-                  <h4 key={elem.id}>
-                    <li>{elem}</li>
-                  </h4>
-                );
-              })}
-            </ul>
-          ) : (
-            <h4> No results </h4>
-          )}
-        </Info2>
-      </Container>
+      <div className="form-group-collection">
+        <input
+          onChange={(e) => {
+            const test = example?.filter((item) => {
+              return item.toLowerCase().includes(e.target.value.toLowerCase());
+            });
+            setData(test);
+            setSearch(e.target.value);
+          }}
+          type="text"
+          value={search}
+        />
+      </div>
+      <StyledButton type="reset" defaultValue="Reset" onClick={reset}>
+        Reset filter
+      </StyledButton>
+
+      <Item>
+        <h3>My tasks:</h3>
+        {data?.length > 0 ? (
+          <ul>
+            {data.map((elem) => {
+              return (
+                <h4 key={elem.id}>
+                  <li>{elem}</li>
+                </h4>
+              );
+            })}
+          </ul>
+        ) : (
+          <h4> No results </h4>
+        )}
+      </Item>
     </Fragment>
   );
 };
