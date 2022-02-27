65d2575efe4551b4c9553e7b3b368505207a774f
Your Name <you@example.com>
2022-02-27 21:25:42 +0100

    add env.dev.customer

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,3 +5,5 @@ export const LOCAL_SESSION_DURATION_SEC = 15 * 60;
 export const ENVS = actionNames(['PRODUCTION', 'DEVELOPMENT', 'TESTING', 'QA'], valueToLower);
 export const APP_ENVS = actionNames(['LOCAL', 'TESTING'], valueToLower);
 export const APP_CHANNELS = actionNames(['STAFF', 'CUSTOMER'], valueToLower);
+export const IS_STAFF = process.env.REACT_APP_CHANNEL_TYPE === 'staff';
+export const IS_CUSTOMER = process.env.REACT_APP_CHANNEL_TYPE === 'customer';
