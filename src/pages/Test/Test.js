91e011f99ef77b30ed5b249d29ee3e9b2a455963
Your Name <you@example.com>
2022-05-11 12:32:01 +0200

    remove search

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,32 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
-import {Search} from 'components/Search';
 
 import 'react-loading-skeleton/dist/skeleton.css';
 import {fetchTestData} from './actions';
 import {testDataSelector, testLoadingSelector} from './selectors';
 import {contentContainer} from './Test.style';
-import constants from './constants';
-
-export const doesQueryIncludeTextPart = (text, query) =>
-  text?.toUpperCase()?.includes(query?.toUpperCase()) || false;
-
-export const getSearchResult = (query, data) =>
-  data?.map(({character, content}) => ({
-    character,
-    content: content
-      .map(({title, text}) =>
-        doesQueryIncludeTextPart(title, query) || doesQueryIncludeTextPart(text, query)
-          ? {title, text}
-          : null,
-      )
-      .filter((item) => !!item),
-  })) || [];
 
 const Test = () => {
-  const [searchResults, setSearchResults] = useState(constants.fullContent);
   const dispatch = useDispatch();
 
   const testData = useSelector((state) => testDataSelector(state));
@@ -37,30 +19,10 @@ const Test = () => {
     dispatch(fetchTestData());
   }, [dispatch]);
 
-  const handleSearch = (query) => {
-    const results = getSearchResult(query, constants.fullContent);
-    setSearchResults(results);
-  };
-
   const componentContent = testLoadingExample ? (
     <Skeleton count={100} />
   ) : (
     <section data-testid="test-container">
-      <Search onSearch={handleSearch} />
-      {searchResults?.map(({character, content}, i) =>
-        content?.length ? (
-          <section key={i}>
-            <h2>{character}</h2>
-            {content.map(({title, text}, j) => (
-              <article key={j}>
-                <h3>{title}</h3>
-                <p>{text}</p>
-              </article>
-            ))}
-          </section>
-        ) : null,
-      )}
-      <p />
       {testData?.length > 0 &&
         testData.map((user) => (
           <div key={user._id}>
